a809997ba4c4e0989d503709d5ddbc8c5776fe78
Anmol Raibhandare <raibhandare.anmol@gmail.com>
2020-08-31 09:55:05 -0700

    Data Model Created.

---
+++ VirtualTouristApp/DataController.swift
@@ -0,0 +1,18 @@
+//
+//  DataController.swift
+//  VirtualTouristApp
+//
+//  Created by Anmol Raibhandare on 8/31/20.
+//  Copyright © 2020 Anmol Raibhandare. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    
+    
+    
+    
+    
+}
