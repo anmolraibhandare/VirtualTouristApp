8bb3024fef0b9d34ae0ceed5a8410acff058d8fa
Anmol Raibhandare <raibhandare.anmol@gmail.com>
2020-08-31 12:23:15 -0700

    Core Data added in PhotosViewController.

--- VirtualTouristApp/Controller/PhotoViewController.swift
+++ VirtualTouristApp/Controller/PhotoViewController.swift
@@ -11,7 +11,15 @@ import UIKit
 import CoreData
 import MapKit
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UICollectionViewDelegate ,UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
     
     // MARK: IBOutlets
 
@@ -20,6 +28,81 @@ class PhotoViewController: UIViewController {
     @IBOutlet weak var newCollection: UIButton!
     @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var noPhotos: UILabel!
+    
+    var fetchedResultsController:NSFetchedResultsController<Photo>!
+    let spacing:CGFloat = 5
+    let totalCells:Int = 25
+    var currentPin: Pin!
+    
+    fileprivate func setUpFetchedResultsController() {
+        let fetchRequest: NSFetchRequest<Photo> = Photo.fetchRequest()
+        
+        let predicate = NSPredicate(format: "pin == %@", currentPin)
+        fetchRequest.predicate = predicate
+        
+        fetchRequest.sortDescriptors = []
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: DataController.shared.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        do{
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("the fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Flowlayout
+        
+        let gap = 2.0
+        let dimension = (Double(self.view.frame.size.width) - (2 * gap)) / 3.0
+
+        flowLayout.minimumLineSpacing = spacing
+        flowLayout.minimumInteritemSpacing = spacing
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+
+        // Collection view delegate and data source assigned to self
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        // new collection button is displayed
+        newCollection.isHidden = false
+        noPhotos.isHidden = true
+
+        collectionView.allowsMultipleSelection = true
+        
+        
+//        addAnnotation()
+
+//        // display results
+//        let savedPhoto = loadSavedPhotos()
+//        if savedPhoto != nil && savedPhoto?.count != 0 {
+//            photos = savedPhoto!
+//            displaySavedResult()
+//        } else {
+//            displayNewResult()
+//        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpFetchedResultsController()
+
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        fetchedResultsController = nil
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        noPhotos.isHidden = !editing
+    }
+    
+    
+    
+    
     
     
     
